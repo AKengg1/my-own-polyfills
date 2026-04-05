23887b9200b54bacffc03fe2a646936f03017b37
AKengg1 <ak72532961@gmail.com>
2026-04-05 10:13:23 +0530

    add addTen function

--- array-polyfills.js
+++ array-polyfills.js
@@ -41,11 +41,22 @@ Array.prototype.myFilter = function (callback) {
 console.log([1, 2, 3, 4].myFilter((n) => n % 2 === 0));
 
 //My reduce
-Array.prototype.myReduce=function(callback,initialval){
-let acc=initialval
+Array.prototype.myReduce = function (callback, initialval) {
+  let acc = initialval;
   for (let i = 0; i < this.length; i++) {
-acc=callback(acc,this[i])
-}
-return acc
-}
-console.log([1, 2, 3, 4].myReduce((acc,current)=>acc+current,10))
+    acc = callback(acc, this[i]);
+  }
+  return acc;
+};
+console.log([1, 2, 3, 4].myReduce((acc, current) => acc + current, 10));
+
+//adding 10 to every element
+Array.prototype.addTen = function () {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    arr.push(this[i] + 10);
+  }
+  return arr;
+};
+
+console.log([1, 2, 3].addTen())
